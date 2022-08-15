80cd7c3775bf7ab52a03946c150a4b1365016303
bouzianes <fcbdreams1991@gmail.com>
2022-08-15 16:33:25 -0400

    styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbars from "./components/TopBar";
+import Background from "./components/BackGround";
+import Columns from "./components/StoreListing";
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
+  integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
+  crossorigin="anonymous"
+/>;
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Navbars />
+      <Background />
+      <Columns />
     </div>
   );
 }
